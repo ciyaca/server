102c97936ac968d060de464447fe96a0e56cdda8
UgOrange <v1mer@protonmail.com>
2020-05-29 00:09:40 +0800

    add id2username

--- account.cpp
+++ account.cpp
@@ -1,6 +1,6 @@
 #include "account.hpp"
 #include "sql/sql.hpp"
-
+#include<cstdlib>
 
 using namespace std;
 
@@ -58,7 +58,50 @@ int regist ( string username, string password ) {
     }
 
 }
-
+int username2userID(string username)
+{
+    string temp = "NULL";
+    string queryStr="SELECT userid FROM user WHERE user_name = "+username+";";
+    mysql_query( &ciyacaSQL, queryStr.c_str() );
+    res_ptr = mysql_store_result(connect);
+     if(res_ptr!=NULL)
+     {
+          result_row = mysql_fetch_row(res_ptr);
+            if(result_row!=NULL)
+            {
+                temp = result_row[0];
+            }
+            mysql_free_result(res_ptr);
+     }
+     else{
+            //查询结果空
+            return 0; 
+        }
+    int num=atoi(temp.c_str());
+    return num;
+}
+int userID2username(string userid)
+{
+    string temp = "NULL";
+    string queryStr="SELECT user_name FROM user WHERE userid = "+userid+";";
+    mysql_query( &ciyacaSQL, queryStr.c_str() );
+    res_ptr = mysql_store_result(connect);
+     if(res_ptr!=NULL)
+     {
+          result_row = mysql_fetch_row(res_ptr);
+            if(result_row!=NULL)
+            {
+                temp = result_row[0];
+            }
+            mysql_free_result(res_ptr);
+     }
+     else{
+            //查询结果空
+            return 0; 
+        }
+    int num=atoi(temp.c_str());
+    return num;
+}
 
 int login ( string username, string password ) {
 
--- account.hpp
+++ account.hpp
@@ -31,7 +31,8 @@ int login ( string username, string password );
 int newGroup ( string username, string groupName );
 int joinGroup ( string username, string groupName );
 int exitGroup ( string username, string groupName );
-
+int username2userID(string username);
+int userID2username(string username);
 
 extern map<string, string> accounts;
 extern map<string, FeverRPC::Caller> connections;
