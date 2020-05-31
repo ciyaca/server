dda1fe9c8e3c71cdfe39260c6ec1dd26d0d3af31
IZAYOIGitHub <1906826229@qq.com>
2020-05-31 09:50:39 +0800

    delete .vscode

--- bbs.cpp
+++ bbs.cpp
@@ -6,41 +6,59 @@
 using namespace std;
 
 
-int postID = 0;
-map<string, map<string, string>> postMap;
+int post ( string postBody ) {
 
+    string queryStr = "INSERT INTO posts (post_string) VALUES ('" + postBody + "');";
 
-int post ( string username, string title, string body ) {
-
-    postMap[title][username] = body;
-
-    cout<<"new post: [ "<<title<<" : "<<body<<" ]"<<endl;
-    return 0;
-
-}
-
-
-int replyPost ( string username, string title, string body ) {
-
-    postMap[title][username] = body;
-
-    cout<<"new replyPost: [ "<<title<<" : "<<body<<" ]"<<endl;
-    return 0;
+    if( mysql_query( &ciyacaSQL, queryStr.c_str() ) ){
+        cout<<"failed to post: [ "<<title<<" : "<<body<<" ]"<<endl;
+        return -1;
+    } else {
+        cout<<"new post: [ "<<title<<" : "<<body<<" ]"<<endl;
+        return 1;
+    }
 
 }
 
 
-string checkPosts( string username, int quantity ) {
+string checkPostFromID( int post_id ){
 
-    map<string, map<string, string>> reqPostMap;
-    reqPostMap.insert( postMap.begin(), postMap.end() );  // test only waiting for debug
+    string queryStr = "SELECT post_string FROM posts WHERE post_id=" + to_string( post_id ) + ";";
 
-    stringstream buffer;
-    msgpack::pack( buffer, reqPostMap );
+    if( mysql_query( &ciyacaSQL, queryStr.c_str() ) ){
+        cout<<"check post failed!"<<endl;
+        return "chekc post failed!";
+    } else {
+        MYSQL_RES* result = mysql_store_result( &ciyacaSQL );
+        MYSQL_ROW row = mysql_fetch_row( result );
+        string post_string = row[0];
+        mysql_free_result( result );
+        cout<<"check post : "<<post_string<<endl;
+        return post_string;
+    }
 
-    auto conn = connections.find( username );
-    conn->second.call<int>( "recvPost", buffer.str() );
+}
 
-    return 0;
+
+string checkPosts( int quality ){
+
+    string queryStr = "SELECT * FROM posts ORDER BY post_id DESC 0," + to_string( quality ) + ";";
+
+    if( mysql_query( &ciyacaSQL, queryStr.c_str() ) ){
+        cout<<"check posts failed!"<<endl;
+        return "check posts failed!";
+    } else {
+        MYSQL_RES* result = mysql_store_result( &ciyacaSQL );
+        string sumPostsString = "";
+        
+        for ( int i = 0; i<mysql_num_rows( result ); i++ ) {
+            MYSQL_ROW row = mysql_fetch_row( result );
+            sumPostsString += row[0];   // post_id
+            sumPostsString += row[1];   // post_string
+        }
+
+        mysql_free_result( result );
+        return sumPostsString;
+    }
 
 }
--- im.cpp
+++ im.cpp
@@ -4,36 +4,39 @@
 using namespace std;
 
 
-int sendMessage ( string sourceName, string targetName, string message ) {
+int sendMessage ( int flag, string sourceName, string targetName, string message ) {
 
-    auto iter = connections.find( targetName );
-    iter->second.call<int>( "recvMessage", sourceName, message );
+    if( flag == 0 ){
 
-    cout<<"from: "<<sourceName<<"; to: "<<targetName<<endl<<"[ "<<message<<" ]"<<endl;
-    return 0;
-    
-}
+        auto iter = connections.find( targetName );
+        iter->second.call<int>( "recvMessage", sourceName, message );
 
+        cout<<"from: "<<sourceName<<"; to: "<<targetName<<endl<<"[ "<<message<<" ]"<<endl;
+        return 0;
 
-int boardcast ( string sourceName, string targetGroupName, string message ) {
-
-    string queryStr = "SELECT username FROM grp_usr WHERE groupname='" + targetGroupName + "';";
-
-    if( mysql_query( &ciyacaSQL, queryStr.c_str() ) ){
-        cout<<targetGroupName<<" not exist!"<<endl;
-        return -1;
     } else {
-        MYSQL_RES* result = mysql_store_result( &ciyacaSQL );
-        
-        for ( int i = 0; i<mysql_num_rows( result ); i++ ) {
-            MYSQL_ROW row = mysql_fetch_row( result );
-            auto iter = connections.find( row[0] );
-            iter->second.call<int>( "recvMessage", sourceName, targetGroupName, message );
-            cout<<"From: "<<sourceName<<" To: "<<row[0]<<endl<<"[ "<<message<<" ]"<<endl;
+
+        string queryStr = "SELECT username FROM grp_usr WHERE groupname='" + targetName + "';";
+
+        if( mysql_query( &ciyacaSQL, queryStr.c_str() ) ){
+            cout<<targetName<<" not exist!"<<endl;
+            return -1;
+        } else {
+            MYSQL_RES* result = mysql_store_result( &ciyacaSQL );
+            
+            for ( int i = 0; i<mysql_num_rows( result ); i++ ) {
+                MYSQL_ROW row = mysql_fetch_row( result );
+                auto iter = connections.find( row[0] );
+                iter->second.call<int>( "recvMessage", sourceName, targetName, message );
+                cout<<"From: "<<sourceName<<" To: "<<row[0]<<endl<<"[ "<<message<<" ]"<<endl;
+            }
+
+            mysql_free_result( result );
+            return 1;
         }
 
-        mysql_free_result( result );
-        return 1;
     }
 
 }
+
+
